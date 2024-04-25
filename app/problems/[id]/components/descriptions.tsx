96ba47e8e80c02a719f6c0e592967b3f5239c845
phuc-create <phuc82908@gmail.com>
2024-04-25 14:38:04 +0700

    added scroll component

--- app/problems/[id]/components/descriptions.tsx
+++ app/problems/[id]/components/descriptions.tsx
@@ -4,6 +4,7 @@ import { Badge } from '../../../../components/ui/badge'
 import { CircleCheckBig } from 'lucide-react'
 import { TestCases } from '../../stores/problem-types'
 import { useProblem } from '../context'
+import { ScrollArea } from '../../../../components/ui/scroll-area'
 
 type DescriptionsPageProps = {
   children?: React.ReactNode
@@ -48,49 +49,55 @@ const DescriptionsPage: React.FC<DescriptionsPageProps> = ({ children }) => {
   const { problem, problemLocal } = useProblem()
   if (!problem || !problemLocal) return null
   return (
-    <div className="relative flex h-full w-full flex-col overflow-hidden overflow-y-scroll rounded-md border p-2">
-      <span className="mb-4 text-2xl font-medium">
-        {problem.number + '. ' + problem.title}
-      </span>
-      <div className="mb-3 flex w-full items-center justify-start">
-        <Badge variant={problem.level} className="mr-4">
-          <span className="capitalize">{problem.level}</span>
-        </Badge>
-        <CircleCheckBig className="h-5 w-5 font-bold" color="green" />
-      </div>
-      <section className="h-auto">
-        <div
-          className="felx h-full w-full flex-col gap-4"
-          data-track-load="description_content"
-        >
-          <div
-            dangerouslySetInnerHTML={{ __html: problem.descriptions || '' }}
-          ></div>
-          <br />
-
-          {problemLocal?.testcases.map((example, index) => {
-            return (
-              <ExampleBlock key={example.id} example={example} index={index} />
-            )
-          })}
-
-          <p>&nbsp;</p>
-          <strong>Constraints:</strong>
-          <ul
-            className="list-disc"
-            dangerouslySetInnerHTML={{ __html: problem.constraints || '' }}
-          ></ul>
-        </div>
-        {children}
-      </section>
-      <p className="mt-4">Categories: </p>
-      <div className="mt-2 flex items-center justify-start gap-4">
-        {problem.categories?.map(cate => (
-          <Badge key={cate} variant="secondary">
-            {cate}
+    <div className="relative flex h-full w-full flex-col rounded-md border py-2 pl-2">
+      <ScrollArea>
+        <span className="mb-4 text-2xl font-medium">
+          {problem.number + '. ' + problem.title}
+        </span>
+        <div className="mb-3 flex w-full items-center justify-start">
+          <Badge variant={problem.level} className="mr-4">
+            <span className="capitalize">{problem.level}</span>
           </Badge>
-        ))}
-      </div>
+          <CircleCheckBig className="h-5 w-5 font-bold" color="green" />
+        </div>
+        <section className="h-auto">
+          <div
+            className="felx h-full w-full flex-col gap-4"
+            data-track-load="description_content"
+          >
+            <div
+              dangerouslySetInnerHTML={{ __html: problem.descriptions || '' }}
+            ></div>
+            <br />
+
+            {problemLocal?.testcases.map((example, index) => {
+              return (
+                <ExampleBlock
+                  key={example.id}
+                  example={example}
+                  index={index}
+                />
+              )
+            })}
+
+            <p>&nbsp;</p>
+            <strong>Constraints:</strong>
+            <ul
+              className="list-disc"
+              dangerouslySetInnerHTML={{ __html: problem.constraints || '' }}
+            ></ul>
+          </div>
+          {children}
+        </section>
+        <p className="mt-4">Categories: </p>
+        <div className="mt-2 flex items-center justify-start gap-4">
+          {problem.categories?.map(cate => (
+            <Badge key={cate} variant="secondary">
+              {cate}
+            </Badge>
+          ))}
+        </div>
+      </ScrollArea>
     </div>
   )
 }
--- app/problems/[id]/components/test-cases.tsx
+++ app/problems/[id]/components/test-cases.tsx
@@ -10,6 +10,7 @@ import { Input } from '../../../../components/ui/input'
 import { useProblem } from '../context'
 import TestcasesBoard from './test-cases-board'
 import TestCasesTab from './test-cases-tab'
+import { ScrollArea } from '../../../../components/ui/scroll-area'
 
 type TestCasesPageProps = {
   name?: string | null
@@ -21,47 +22,49 @@ const TestCasesPage: React.FC<TestCasesPageProps> = () => {
   const cases = problemLocal?.testcases
 
   return (
-    <div className="relative h-full w-full">
-      <div className="relative h-full w-full overflow-hidden overflow-y-scroll rounded-md border p-4">
-        <Tabs defaultValue={cases[0].id}>
-          <TabsList className="bg-transparent">
+    <div className="relative h-full w-full rounded-md border">
+      <ScrollArea className="h-full w-full">
+        <div className="p-4">
+          <Tabs defaultValue={cases[0].id}>
+            <TabsList className="bg-transparent">
+              {!results.length ? (
+                cases.map((c, i) => (
+                  <TabsTrigger key={c.id} value={c.id} className="mr-4 border">
+                    Case {i + 1}
+                  </TabsTrigger>
+                ))
+              ) : (
+                <TestCasesTab />
+              )}
+            </TabsList>
+
             {!results.length ? (
-              cases.map((c, i) => (
-                <TabsTrigger key={c.id} value={c.id} className="mr-4 border">
-                  Case {i + 1}
-                </TabsTrigger>
-              ))
+              cases.map(c => {
+                const { input } = c
+                const varFormat = Object.keys(input)
+                  .map(k => `${k} =`)
+                  .join(', ')
+                const inputFormat = Object.values(input).join(', ')
+
+                return (
+                  <TabsContent key={c.id} value={c.id}>
+                    <div className="flex flex-col">
+                      <span>{varFormat}</span>
+                      <Input
+                        disabled
+                        value={inputFormat.toString()}
+                        className="overflow-hidden truncate text-ellipsis text-nowrap"
+                      />
+                    </div>
+                  </TabsContent>
+                )
+              })
             ) : (
-              <TestCasesTab />
+              <TestcasesBoard />
             )}
-          </TabsList>
-
-          {!results.length ? (
-            cases.map(c => {
-              const { input } = c
-              const varFormat = Object.keys(input)
-                .map(k => `${k} =`)
-                .join(', ')
-              const inputFormat = Object.values(input).join(', ')
-
-              return (
-                <TabsContent key={c.id} value={c.id}>
-                  <div className="flex flex-col">
-                    <span>{varFormat}</span>
-                    <Input
-                      disabled
-                      value={inputFormat.toString()}
-                      className="overflow-hidden truncate text-ellipsis text-nowrap"
-                    />
-                  </div>
-                </TabsContent>
-              )
-            })
-          ) : (
-            <TestcasesBoard />
-          )}
-        </Tabs>
-      </div>
+          </Tabs>
+        </div>
+      </ScrollArea>
     </div>
   )
 }
